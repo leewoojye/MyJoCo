f63548b0c2df540c6425e01f3853b248a764b128
leewoojye <whitehole36@naver.com>
2026-05-04 19:05:26 +0900

    Refactor append_joints and compute_position_jacobian: enhance joint handling and include node information

--- sim/model/kinematics/jacobian.py
+++ sim/model/kinematics/jacobian.py
@@ -8,14 +8,14 @@ from sim.model.robot.robot_model import RobotGeometries
 from sim.model.robot.state import RobotState
 
 
-def append_joints(all_joints, node: MuJoCoBodyNode):
+def append_joints(all_joints, node: MuJoCoBodyNode, include_nodes=False):
     # 부모 노드는 1개로 가정
     if node.parent is not None:
-        append_joints(all_joints, node.parent)
+        append_joints(all_joints, node.parent, include_nodes=include_nodes)
 
     # 여러 관절을 갖는 링크를 고려해 매스텝마다 joint 정렬해줌
     for joint in sorted(node.joints, key=lambda joint: joint["qpos_addr"]):
-        all_joints.append(joint)
+        all_joints.append((node, joint) if include_nodes else joint)
 
     return
 
@@ -31,20 +31,34 @@ def compute_position_jacobian(robot: RobotGeometries, link_poses=None):
     pos_ee = target_transform[:3, 3]
 
     all_joints = []
-    append_joints(all_joints, target)
+    # (node, joint) 중 하나만을 배열 원소로 삼을 것인지 결정하는 flag 변수 include_nodes
+    append_joints(all_joints, target, include_nodes=True)
 
     # 자코비안 행렬은 e.e에서 root까지 단일 경로만 고려
     all_S = []
-    for joint in all_joints:  # 회전관절 가정 (추후 수정)
-        q = joint["world_pos"]
-        w = joint["world_axis"]
+    joints = []
+    for node, joint in all_joints:  # 회전관절 가정 (추후 수정)
+        joint_transform = (
+            # 외부에서 link_poses(space transform)를 넘겨받음, 이는 M을 요구하는 FK와 달리 jacobian은 '현재' 상태를 기준으로 열을 구성하기 때문임
+            # 현재 자세의 link_poses가 필요한 이유는 로딩 시 저장된 joint["world_axis"], joint["world_pos"]는 home pose 기준이기 때문임, 로봇이 움직이면 joint 축 방향도 world 기준으로 바뀌는데, 그걸 반영하려면 현재 body transform이 필요함
+            link_poses[node.name] if link_poses is not None else node.world_transform
+        )
+
+        # q_sb = R_sb @ q_b + p_sb  # 점 변환
+        # w_sb = R_sb @ w_b  # 방향 변환
+        q = (
+            joint_transform[:3, :3] @ joint["pos"] + joint_transform[:3, 3]
+        )  # joint_transform[:3, :3] : space-frame transformation(T_sb)
+        w = joint_transform[:3, :3] @ joint["axis"]
         S = unit_screw_axis(q, w, 0, joint["type"])
         v = S[3:, 0]
         J_i = np.cross(w, pos_ee) + v  # np.cross(omega, p_ee - q)
         all_S.append(J_i.reshape(3, 1))  # 회전 성분을 제외한 열벡터 append
+        joints.append(joint)
 
     # 자코비안 열 인덱스와 joint id(qpos_addr)이 일치하지 않을 수 있어 관절 객체 리스트로 같이 반환
-    return np.concatenate(all_S, axis=1), all_joints
+    # space frame 기준 position jacobian
+    return np.concatenate(all_S, axis=1), joints
 
 
 # end-effector(ex. omy의 link6)에 대한 자코비안 행렬 생성
